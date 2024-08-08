b63fecd8e2ea0503914e077b9de88fe32c609991
abdulaziz <abdulaziz125000f@gmail.com>
2024-08-08 12:43:45 +0300

    Abdulaziz

--- Streamlit.py
+++ Streamlit.py
@@ -8,4 +8,6 @@ st.image("labstr.png", caption="Sunrise by the mountains")
 st.image("test2.png", caption="test")
 st.title("سيارات ")
 
+st.markdown("<h1 style='text-align: center; color: red;'>سيارات</h1>", unsafe_allow_html=True)
+
 
