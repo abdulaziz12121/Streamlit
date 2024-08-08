9560eebb7ae7deb22d7b65863594b67f80b3ce0e
abdulaziz <abdulaziz125000f@gmail.com>
2024-08-08 12:11:08 +0300

    Abdulaziz

--- Streamlit.py
+++ Streamlit.py
@@ -4,3 +4,7 @@ import pandas as pd
 import numpy as np
 st.write("good")
 st.image("labstr.png", caption="Sunrise by the mountains")
+
+option = st.selectbox('How would you like to be contacted?',('Email', 'Home phone', 'Mobile phone'))
+
+st.write('You selected:', option)
