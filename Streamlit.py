07b522ef3c9aa74b2f50adf64e9c24a37ee2fc50
abdulaziz <abdulaziz125000f@gmail.com>
2024-08-08 13:10:20 +0300

    Abdulaziz

--- Streamlit.py
+++ Streamlit.py
@@ -2,21 +2,17 @@ import streamlit as st
 
 import pandas as pd
 import numpy as np
-st.write("good")
-st.image("labstr.png", caption="Sunrise by the mountains")
 st.markdown(
     """
     <div style="direction: rtl;">
-        <h2>سيارات</h2>
+        <h1>سيارات</h1>
         
     </div>
     """,
     unsafe_allow_html=True
 )
-st.image("test2.png", caption="test")
-st.title("سيارات ")
 
-st.markdown("<h1 style='text-align: center; color: red;'>سيارات</h1>", unsafe_allow_html=True)
+
 
 # Using HTML and CSS to set text direction to right-to-left
 st.markdown(
