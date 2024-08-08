caeacfcf84025ec03b225e347746d964828e4378
abdulaziz <abdulaziz125000f@gmail.com>
2024-08-08 12:15:15 +0300

    Abdulaziz

--- Streamlit.py
+++ Streamlit.py
@@ -8,3 +8,5 @@ st.image("labstr.png", caption="Sunrise by the mountains")
 option = st.selectbox('How would you like to be contacted?',('Email', 'Home phone', 'Mobile phone'))
 
 st.write('You selected:', option)
+
+st.image("Screenshot 2024-08-08 121307", caption="Sunrise by the mountains")
