0a666c6cae670b64f05637b9f9f92d162d9e8a62
abdulaziz <abdulaziz125000f@gmail.com>
2024-08-08 12:46:43 +0300

    Abdulaziz

--- Streamlit.py
+++ Streamlit.py
@@ -10,4 +10,13 @@ st.title("سيارات ")
 
 st.markdown("<h1 style='text-align: center; color: red;'>سيارات</h1>", unsafe_allow_html=True)
 
-
+# Using HTML and CSS to set text direction to right-to-left
+st.markdown(
+    """
+    <div style="direction: rtl;">
+        <h1>This text is written from right to left</h1>
+        <p>سيارات/p>
+    </div>
+    """,
+    unsafe_allow_html=True
+)
